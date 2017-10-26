cbc75c2c2128768518917e08414b479dbcb0af0d
FERNANDOSERVIDIO <FERNANDO.SERVIDIO@users.noreply.github.com>
2017-10-26 18:27:38 -0300

    Draw Mode
    Agregué una ventana (Draw Mode) que crea una subVentana con un boton, ese instantea el "button" en el canvas con la posicion de la subVentana.

---
+++ Assets/Nodes.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Nodes
+{
+
+    public Rect window;
+    public string name;
+    public List<Nodes> connections;
+
+    public Nodes(string name, int posX = 100, int posY = 100, int width = 200, int height = 50)
+    {
+        this.name = name;
+        window = new Rect(posX, posY, width, height);
+        connections = new List<Nodes>();
+    }
+}
