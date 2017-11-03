52eb54e6eb0e7f2a24d1b10e32da9b80f8afb0fa
AilenTraillou <ailentraillou@gmail.com>
2017-11-03 06:43:21 -0300

    Instance de Texto con Fuente, Color, y Tamaño personalizables

--- Assets/Editor/CounterBuilder.cs
+++ Assets/Editor/CounterBuilder.cs
@@ -8,8 +8,11 @@ public class CounterBuilder : EditorWindow {
 
 
     float timeToRestart = 0;
-    public string demoText = "This is a Test Counter: 0, 1, 2, 3, 4, 5, 6, 7, 8, 9";
+    int fontSize = 0; 
+    
     Font fontStyle;
+    GameObject displayedText;
+    Color fontColor;
 
     [MenuItem("Editor Windows/Counter Builder")]
     static void CreateWindow()
@@ -20,7 +23,9 @@ public class CounterBuilder : EditorWindow {
     }
 	
 	void OnGUI () {
-		
+
+        GUI.skin.font = fontStyle;
+
         Canvas canvas = FindObjectOfType<Canvas>();
 
         minSize = new Vector2(800, 400);
@@ -33,7 +38,6 @@ public class CounterBuilder : EditorWindow {
         EditorGUILayout.HelpBox("In this window you can define a counter to restart to main menu, for example, if player had lose the game", 
             MessageType.Info);
 
-
         EditorGUILayout.EndVertical();
 
         GUILayout.Space(20);
@@ -41,20 +45,40 @@ public class CounterBuilder : EditorWindow {
         EditorGUILayout.BeginHorizontal();
 
         timeToRestart = EditorGUILayout.FloatField("Time to restart", timeToRestart);
+        fontSize = EditorGUILayout.IntField("Font Size int Screen", fontSize);
+
+        EditorGUILayout.EndHorizontal();
+        EditorGUILayout.BeginHorizontal();
 
         fontStyle = (Font)EditorGUILayout.ObjectField("Font Style for Timer", fontStyle, typeof(Font), true);
+        fontColor.a = 255;
+        fontColor = EditorGUILayout.ColorField("Font Color", fontColor);
 
         EditorGUILayout.EndHorizontal();
 
         GUILayout.Space(15);
-        
+
         EditorGUILayout.LabelField("This is a Test Counter: 0, 1, 2, 3, 4, 5, 6, 7, 8, 9", EditorStyles.largeLabel);
+        GUI.skin.font = default(Font);
+
+        GUILayout.Space(15);
 
 
         if (GUILayout.Button("Add Counter to Scene"))
         {
+            displayedText = new GameObject("Text");
+            displayedText.AddComponent<Text>();
+            displayedText.GetComponent<Text>().text = timeToRestart.ToString();
+            displayedText.GetComponent<Text>().alignment = TextAnchor.MiddleCenter;
+            displayedText.GetComponent<RectTransform>().sizeDelta = new Vector2(350, 200);
+            displayedText.GetComponent<Text>().font = fontStyle;
+            displayedText.GetComponent<Text>().fontSize = fontSize;
+            displayedText.GetComponent<Text>().color = fontColor;
 
 
+            displayedText.transform.position = FindObjectOfType<Canvas>().GetComponent<Transform>().position;
+
+            displayedText.transform.parent = FindObjectOfType<Canvas>().GetComponent<Transform>();
 
         }
 
